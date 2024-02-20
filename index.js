d29d64c00dd30655c680b414e31635c0ebbbfcc6
Yashassvi Suhane <87795720+yashuswe@users.noreply.github.com>
2024-02-20 22:37:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function decreament() {
+    count -= 1
+    if (count < 0)
+    {
+        count = 0;
+    }
+    if(count >= 0){
+    countEl.textContent = count
+    }
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
